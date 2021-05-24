5b97694b5f1469c4e0cc5d1f84e319594780aa46
jabooticaba <jabuticaba600@gmail.com>
2021-05-24 19:21:08 +0700

    global variables deleted new tests added

--- api.py
+++ api.py
@@ -2,99 +2,166 @@ import requests
 import json
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 
+
 class PetFriends:
-  def __init__(self):
-    self.base_url = 'https://petfriends1.herokuapp.com/'
+    def __init__(self):
+        self.base_url = 'https://petfriends1.herokuapp.com/'
 
-  def get_api_key(self, email: str, password: str) -> json:
-    """This method allows to get API key which should be used for other API methods and a status of request
-    in JSON format"""
+    def get_api_key(self, email: str, password: str) -> json:
+        """This method allows to get unique API key which should be used for other API methods
+        and a status of request in JSON format.
+        """
+        headers = {
+            'email': email,
+            'password': password,
+        }
 
-    headers = {
-      'email': email,
-      'password': password,
-    }
+        res = requests.get(self.base_url + 'api/key', headers=headers)
+        status = res.status_code
+        result = ''
+        try:
+            result = res.json()
+        except:
+            result = res.text
 
-    res = requests.get(self.base_url+'api/key', headers=headers)
-    status = res.status_code
-    result = ''
+        return status, result
 
-    try:
-      result = res.json()
-    except:
-      result = res.text
+    def list_of_pets(self, auth_key: json, filter: str = '') -> json:
+        """Method allows to get the status of the request and list of pets,
+        which must match the filter and returned in json format. Filter variants: 'my_pets' or ''.
+        """
+        headers = {"auth_key": auth_key['key']}
+        filter = {'filter': filter}
 
-    return status, result
+        res = requests.get(self.base_url + 'api/pets', headers=headers, params=filter)
+        status = res.status_code
+        result = ''
+        try:
+            result = res.json()
+        except:
+            result = res.text
 
-  def list_of_pets(self, auth_key: json, filter:str = '') -> json:
-    """This method allows to get the status of the request and list of pets,
-    which must match the filter and retured in json format."""
+        return status, result
 
-    # headers = {"auth_key": auth_key['key']}
-    headers = {"auth_key": auth_key}
-    filter = {'filter': filter}
+    def add_new_pet(self, auth_key: json, name: str, animal_type: str, age: str, pet_photo) -> json:
+        """Method allows to add information about new pet. Takes API key and information about new pet (Name,
+        animal type, age, path to photo). Response contains the status code and new pet data in json format
+         (including a unique pet id).
+         """
+        data = MultipartEncoder(
+            fields={
+                'name': name,
+                'animal_type': animal_type,
+                'age': age,
+                'pet_photo': (pet_photo, open(pet_photo, 'rb'), 'image/jpeg')
+            })
+        headers = {'auth_key': auth_key['key'], 'Content-Type': data.content_type}
 
-    res = requests.get(self.base_url+'api/pets', headers=headers, params=filter)
-    status = res.status_code
-    result = ''
+        res = requests.post(self.base_url + 'api/pets', data=data, headers=headers)
+        status = res.status_code
+        result = ''
 
-    try:
-      result = res.json()
-    except:
-      result = res.text
+        try:
+            result = res.json()
+        except:
+            result = res.text
 
-    return status, result
+        return status, result
 
-  def add_new_pet(self, auth_key:json, name:str, animal_type:str, age:str, pet_photo) -> json:
-    """This method allows to add information about new pet."""
+    def update_pet_info(self, auth_key, pet_id: str, name: str, animal_type: str, age: str) -> json:
+        """This method allows to update information about pet. Takes API key, pet id and information about
+        new pet (name, animal type, age). Response contains the status code and new pet data in json format
+        (including a unique pet id).
+         """
+        data = MultipartEncoder(
+            fields={
+                'name': name,
+                'animal_type': animal_type,
+                'age': age
+            })
+        headers = {'auth_key': auth_key['key'], 'Content-Type': data.content_type}
 
-    data = MultipartEncoder(
-    fields={
-       'name': name,
-       'animal_type': animal_type,
-       'age': age,
-       'pet_photo': (pet_photo, open(pet_photo, 'rb'), 'image/jpeg')
-    })
-    headers = {'auth_key': auth_key, 'Content-Type': data.content_type} # ['key'] deleted from auth_key
+        res = requests.put(self.base_url + f'/api/pets/{pet_id}', data=data, headers=headers)
+        status = res.status_code
+        result = ''
+        try:
+            result = res.json()
+        except:
+            result = res.text
 
-    res = requests.post(self.base_url+'api/pets', data = data, headers = headers)
-    status = res.status_code
-    result = ''
+        return status, result
 
-    try:
-      result = res.json()
-    except:
-      result = res.text
+    def delete_pet(self, auth_key: json, pet_id: str) -> json:
+        """Method removes pet with pet_id, returns status code"""
 
-    return status, result
+        headers = {'auth_key': auth_key['key']}
 
-  def update_pet_info(self, auth_key:json, pet_id, name: str, animal_type: str, age: str) -> json:
+        res = requests.delete(self.base_url + f'/api/pets/{pet_id}', headers=headers)
+        status = res.status_code
 
-    data = MultipartEncoder(
-    fields={
-       'name': name,
-       'animal_type': animal_type,
-       'age': age       
-    })
-    headers = {'auth_key': auth_key, 'Content-Type': data.content_type} # ['key'] deleted from auth_key
+        return status
 
-    res = requests.put(self.base_url+ f'/api/pets/{pet_id}', data = data, headers = headers)
-    status = res.status_code
-    result = ''
+    def add_new_pet_without_photo(self, auth_key: json, name: str, animal_type: str, age: str) -> json:
+        """This method allows to add information about new pet without photo. Takes API key, and information about
+        new pet (name, animal type, age). Response contains the status code and new pet data in json format
+        (including the unique pet id)."""
+        data = MultipartEncoder(
+            fields={
+                'name': name,
+                'animal_type': animal_type,
+                'age': age
+            })
+        headers = {'auth_key': auth_key['key'], 'Content-Type': data.content_type}
 
-    try:
-      result = res.json()
-    except:
-      result = res.text
+        res = requests.post(self.base_url + 'api/create_pet_simple', data=data, headers=headers)
+        status = res.status_code
+        result = ''
+        try:
+            result = res.json()
+        except:
+            result = res.text
 
-    return status, result
+        return status, result
 
-  def delete_pet(self, auth_key:json, pet_id) -> json:
-    """Delete pet with {pet_id}, returns status code"""
+    def add_photo_of_pet(self, auth_key: json, pet_id: str, pet_photo) -> json:
+        """This method allows to add photo of a pet. Takes API key, pet id and path to pet photo.
+        Response contains the status code and new pet data in json format.
+        """
 
-    headers = {'auth_key': auth_key} # ['key'] deleted from auth_key
+        data = MultipartEncoder(
+            fields={
+                'pet_photo': (pet_photo, open(pet_photo, 'rb'), 'image/jpeg')
+            })
+        headers = {'auth_key': auth_key['key'], 'Content-Type': data.content_type}
 
-    res = requests.delete(self.base_url+ f'/api/pets/{pet_id}', headers = headers)
-    status = res.status_code
-    
-    return status
+        res = requests.post(self.base_url + f'/api/pets/set_photo/{pet_id}', data=data, headers=headers)
+        status = res.status_code
+        result = ''
+
+        try:
+            result = res.json()
+        except:
+            result = res.text
+
+        return status, result
+
+    def add_photo_of_pet_without_photo(self, auth_key: json, pet_id: str) -> json:
+        """This method allows to add photo of a pet. Takes API key, pet id and path to pet photo.
+        Response contains the status code and new pet data in json format.
+        """
+
+        data = MultipartEncoder(
+            fields={
+                            })
+        headers = {'auth_key': auth_key['key'], 'Content-Type': data.content_type}
+
+        res = requests.post(self.base_url + f'/api/pets/set_photo/{pet_id}', data=data, headers=headers)
+        status = res.status_code
+        result = ''
+
+        try:
+            result = res.json()
+        except:
+            result = res.text
+
+        return status, result
--- tests/test_pet_friends.py
+++ tests/test_pet_friends.py
@@ -4,59 +4,96 @@ sys.path.insert(0, '..')
 from api import PetFriends
 from settings import valid_email, valid_password
 
-
-class StoreStuff:
-    """Используется для хранения переменных тест-рана, auth_key, pet_id и т.д."""
-    auth_key = None
-    pet_id = None
-    # first_pet_id = None  # Используется в цикле удаления питомцев
-
-
 pf = PetFriends()
-store_stuff = StoreStuff
 
 
 def test_get_api_for_valid_user(email=valid_email, password=valid_password):
     status, result = pf.get_api_key(email, password)
-    store_stuff.auth_key = result['key']
+
     assert status == 200
     assert 'key' in result
 
 
 def test_get_list_of_pets_with_valid_key(filter='my_pets'):
-    # _, auth_key = pf.get_api_key(valid_email, valid_password)
-    auth_key = store_stuff.auth_key
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+
     status, result = pf.list_of_pets(auth_key, filter)
     assert status == 200
     assert len(result['pets']) > 0
 
-    # store_stuff.first_pet_id = result['pets'][0]['id']  # Используется для очистки списка питомцев в цикле
-    # print(store_stuff.first_pet_id)
-
 
 def test_add_new_pet_with_valid_data_and_photo(name="Пушистик", animal_type="Racoon",
                                                age='4', pet_photo="../images/animal.jpg"):
-    # _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
 
-    status, result = pf.add_new_pet(store_stuff.auth_key, name, animal_type, age, pet_photo)  # auth_key replaced
+    status, result = pf.add_new_pet(auth_key, name, animal_type, age, pet_photo)  # auth_key replaced
     assert status == 200
     assert result['name'] == "Пушистик"
     assert result['age'] == '4'
-    store_stuff.pet_id = result['id']
 
 
 def test_update_information_about_pet_with_valid_data(name='Полосатик', animal_type="Енот", age='4'):
-    status, result = pf.update_pet_info(store_stuff.auth_key, store_stuff.pet_id, name, animal_type, age)
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.list_of_pets(auth_key, 'my_pets')
+
+    if len(my_pets['pets']) == 0:
+        pf.add_new_pet_without_photo(auth_key, "Тест-кот", "кот", "2")
+        _, my_pets = pf.list_of_pets(auth_key, "my_pets")
+
+    status, result = pf.update_pet_info(auth_key, my_pets['pets'][0]['id'], name, animal_type, age)
     assert status == 200
     assert result['animal_type'] == 'Енот'
 
 
-#   def test_delete_pet_valid_id():  # Удалить 10 питомцев в цикле pytest --repeat-scope=session --count=10,
-#  закомментить тесты 3, 4
-#   status = pf.delete_pet(store_stuff.auth_key, store_stuff.first_pet_id)
-#   assert status == 200
-
-
 def test_delete_pet_valid_id():
-    status = pf.delete_pet(store_stuff.auth_key, store_stuff.pet_id)
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.list_of_pets(auth_key, 'my_pets')
+
+    if len(my_pets['pets']) == 0:
+        pf.add_new_pet_without_photo(auth_key, "Тест-кот", "кот", "2")
+        _, my_pets = pf.list_of_pets(auth_key, "my_pets")
+
+    pet_id = my_pets['pets'][0]['id']
+    status = pf.delete_pet(auth_key, pet_id)
+    _, my_pets = pf.list_of_pets(auth_key, "my_pets")
+
     assert status == 200
+    assert pet_id not in my_pets.values()
+
+
+def test_add_new_pet_without_photo_valid_data(name="Продам гитару", animal_type="Аккустика",
+                                              age='4'):
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+
+    status, result = pf.add_new_pet_without_photo(auth_key, name, animal_type, age)  # auth_key replaced
+    assert status == 200
+    assert result['name'] == "Продам гитару"
+    assert result['age'] == '4'
+
+def test_add_photo_of_pet_valid_data(pet_photo="../images/guitar.jpg"):
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.list_of_pets(auth_key, 'my_pets')
+
+    if len(my_pets['pets']) == 0:
+        pf.add_new_pet_without_photo(auth_key, "Тест-кот", "кот", "2")
+        _, my_pets = pf.list_of_pets(auth_key, "my_pets")
+
+    pet_id = my_pets['pets'][0]['id']
+    status, result = pf.add_photo_of_pet(auth_key, pet_id, pet_photo)
+    assert status == 200
+    assert result['pet_photo']
+    # pf.delete_pet(auth_key, pet_id)
+
+
+def test_add_photo_of_pet_no_photo():
+    _, auth_key = pf.get_api_key(valid_email, valid_password)
+    _, my_pets = pf.list_of_pets(auth_key, 'my_pets')
+
+    if len(my_pets['pets']) == 0:
+        pf.add_new_pet_without_photo(auth_key, "Тест-кот", "кот", "2")
+        _, my_pets = pf.list_of_pets(auth_key, "my_pets")
+
+    pet_id = my_pets['pets'][0]['id']
+    status, result = pf.add_photo_of_pet_without_photo(auth_key, pet_id)
+    assert status == 400
+
